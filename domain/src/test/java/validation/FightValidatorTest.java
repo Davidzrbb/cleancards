1c93c364ac7000d055b0f76366d07d48f41571ff
QuentinCoqueran <coqueranquentin@gmail.com>
2023-02-25 17:53:20 +0100

    wip tests

--- domain/src/test/java/validation/FightValidatorTest.java
+++ domain/src/test/java/validation/FightValidatorTest.java
@@ -3,23 +3,17 @@ package validation;
 import arch.hex.domain.ApplicationError;
 import arch.hex.domain.functional.model.Deck;
 import arch.hex.domain.functional.model.Hero;
-import arch.hex.domain.functional.model.Player;
 import arch.hex.domain.functional.service.deck_services.DeckFinderByIdService;
 import arch.hex.domain.functional.service.validation.FightValidator;
 import io.vavr.control.Option;
 import io.vavr.control.Validation;
-import lombok.RequiredArgsConstructor;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -36,18 +30,18 @@ class FightValidatorTest {
     String idEnemy = "idEnemy";
 
     @Test
-    public void shouldReturnInvalidWhenDeckNotFound() {
+    void shouldReturnInvalidWhenDeckNotFound() {
         when(deckFinderByIdService.findById(idAlly)).thenReturn(Option.none());
         when(deckFinderByIdService.findById(idEnemy)).thenReturn(Option.none());
 
         Validation<ApplicationError, List<Deck>> result = fightValidator.validateFight(idAlly, idEnemy);
 
-        assertEquals(result.isInvalid(), true);
-        assertEquals(result.getError().context(), "Deck not found");
+        assertEquals(true, result.isInvalid());
+        assertEquals("Deck not found", result.getError().context());
     }
 
     @Test
-    public void shouldReturnInvalidWhenHeroNotFound() {
+    void shouldReturnInvalidWhenHeroNotFound() {
         Deck deckAlly = Deck.builder().idDeck(idAlly).hero(null).build();
         Deck deckEnemy = Deck.builder().idDeck(idEnemy).hero(null).build();
         when(deckFinderByIdService.findById(idAlly)).thenReturn(Option.of(deckAlly));
@@ -55,12 +49,12 @@ class FightValidatorTest {
 
         Validation<ApplicationError, List<Deck>> result = fightValidator.validateFight(idAlly, idEnemy);
 
-        assertEquals(result.isInvalid(), true);
-        assertEquals(result.getError().context(), "Hero not found");
+        assertEquals(true,result.isInvalid());
+        assertEquals( "Hero not found", result.getError().context());
     }
 
     @Test
-    public void shouldReturnInvalidWhenHeroLevelIsTooHigh() {
+    void shouldReturnInvalidWhenHeroLevelIsTooHigh() {
         Hero heroAlly = Hero.builder().level(3).build();
         Hero heroEnemy = Hero.builder().level(2).build();
         Deck deckAlly = Deck.builder().idDeck(idAlly).hero(heroAlly).build();
@@ -70,12 +64,12 @@ class FightValidatorTest {
 
         Validation<ApplicationError, List<Deck>> result = fightValidator.validateFight(idAlly, idEnemy);
 
-        assertEquals(result.isInvalid(), true);
-        assertEquals(result.getError().context(), "Hero level is too high");
+        assertEquals(true, result.isInvalid());
+        assertEquals("Hero level is too high", result.getError().context());
     }
 
     @Test
-    public void shouldReturnInvalidWhenPlayerNotFound() {
+    void shouldReturnInvalidWhenPlayerNotFound() {
         Deck deckAlly = Deck.builder().idDeck(idAlly).player(null).build();
         Deck deckEnemy = Deck.builder().idDeck(idEnemy).player(null).build();
         when(deckFinderByIdService.findById(idAlly)).thenReturn(Option.of(deckAlly));
@@ -83,7 +77,7 @@ class FightValidatorTest {
 
         Validation<ApplicationError, List<Deck>> result = fightValidator.validateFight(idAlly, idEnemy);
 
-        assertEquals(result.isInvalid(), true);
-        assertEquals(result.getError().context(), "Player not found");
+        assertEquals(true, result.isInvalid());
+        assertEquals("Player not found", result.getError().context());
     }
 }
